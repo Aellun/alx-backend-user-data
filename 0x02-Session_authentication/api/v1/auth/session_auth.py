ee81605572247095108699f16982398e4754e234
aellun <okello.k.otieno@gmail.com>
2024-09-06 01:16:43 +0300

    updated session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,12 @@
 Module for session-based authentication.
 """
 
-from .auth import Auth
+from flask import jsonify, request, abort
 from models.user import User
+from api.v1.views import app_views
 from uuid import uuid4
+import os
+from api.v1.app import auth
 
 
 class SessionAuth(Auth):
@@ -29,7 +32,7 @@ class SessionAuth(Auth):
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """
-        Retrieves the user ID associated with a given session ID.
+        Fetches the user ID associated with a given session ID.
         Returns:
             str: The associated user ID,
             or None if session ID is invalid or not found.
@@ -40,7 +43,7 @@ class SessionAuth(Auth):
 
     def current_user(self, request=None):
         """
-        fetchs the current user based on the session cookie from the request.
+        Fetchs the current user based on the session cookie from the request.
         Returns:
             User: The user associated with the session,
             or None if no valid session is found.
@@ -66,3 +69,50 @@ class SessionAuth(Auth):
             return False
         del self.user_id_by_session_id[session_cookie]
         return True
+
+
+@app_views.route('/auth_session/login/',
+                 methods=['POST'], strict_slashes=False)
+def login():
+    """
+    POST /api/v1/auth_session/login: Create a session for the user
+    if credentials are valid.
+
+    Returns:
+        A JSON response with the user details and a session cookie set.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # Email validation
+    if email is None or email == '':
+        return jsonify({"error": "email missing"}), 400
+
+    # Password validation
+    if password is None or password == '':
+        return jsonify({"error": "password missing"}), 400
+
+    # Search for the user by email
+    try:
+        users = User.search({"email": email})
+    except Exception as e:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    if len(users) == 0:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    user = users[0]
+
+    # Password validation
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    # Create session and generate session ID
+    session_id = auth.create_session(user.id)
+
+    # Generate a response and set the session cookie
+    response = jsonify(user.to_json())
+    session_name = os.getenv('SESSION_NAME')
+    response.set_cookie(session_name, session_id)
+
+    return response
